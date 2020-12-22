78488e86dc0ee6a01b0f52037d414ad9d89f03ba
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2020-12-22 15:41:20 -0700

    Changed the number of guesses to 4.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,20 @@ namespace _4Guesses
     {
         static string Guess(int answer, int guesses)
         {
-            Console.WriteLine("I am thinking of a number between 1 and 10 try to guess it");
+            Console.WriteLine("I am thinking of a number between 1 and 10 try to guess it!");
             var currentGuess = int.Parse(Console.ReadLine());
             if(currentGuess == answer)
             {
                 return("You win!");
             }
-            if(currentGuess != answer && )
+            else if(currentGuess != answer && guesses < 3)
             {
                 Console.WriteLine("That is wrong I am not thinking of {0}. Try again!", currentGuess);
                 return(Guess(answer,++guesses));
+            }
+            else
+            {
+                return("You Lose!");
             }
         }
         static void Main(string[] args)
@@ -24,6 +28,7 @@ namespace _4Guesses
             var answer = random.Next(1, 11);
             var guesses = 0;
             var result = Guess(answer, guesses);
+            Console.WriteLine("{0} I was thinking of {1}",result, answer);
         }
     }
 }
