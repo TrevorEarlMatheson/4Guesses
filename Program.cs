fe71a712b0642068f9b715739b7aa1bc3c80cabd
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2020-12-22 15:33:52 -0700

    Adding some logic to check the users guess.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,26 @@ namespace _4Guesses
 {
     class Program
     {
+        static string Guess(int answer, int guesses)
+        {
+            Console.WriteLine("I am thinking of a number between 1 and 10 try to guess it");
+            var currentGuess = int.Parse(Console.ReadLine());
+            if(currentGuess == answer)
+            {
+                return("You win!");
+            }
+            if(currentGuess != answer && )
+            {
+                Console.WriteLine("That is wrong I am not thinking of {0}. Try again!", currentGuess);
+                return(Guess(answer,++guesses));
+            }
+        }
         static void Main(string[] args)
         {
             var random = new Random();
             var answer = random.Next(1, 11);
-            
+            var guesses = 0;
+            var result = Guess(answer, guesses);
         }
     }
 }
