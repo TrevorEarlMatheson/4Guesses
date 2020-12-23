34c7fd8a7b67a94000aeff013f533e6531227b89
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2020-12-23 09:22:18 -0700

    Added some comments for code clarity.

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace _4Guesses
             Console.WriteLine("I am thinking of a number between 1 and 10 try to guess it!");
             try
             {
-                var currentGuess = int.Parse(Console.ReadLine());
+                var currentGuess = int.Parse(Console.ReadLine()); // Return if they get the answer right.
                 if(currentGuess == answer)
                 {
                     return("You win!");
                 }
-                else if(currentGuess != answer && guesses < 3)
+                else if(currentGuess != answer && guesses < 3) //If user is wrong and they still have more guesses have the user guess again.
                 {
                     Console.WriteLine("That is wrong I am not thinking of {0}. Try again!", currentGuess);
                     return(Guess(answer,++guesses));
                 }
-                else
+                else // If they have no more guesses return a lose prompt.
                 {
                     return("You Lose!");
                 }
@@ -36,7 +36,7 @@ namespace _4Guesses
             var answer = random.Next(1, 11);
             var guesses = 0;
             var result = Guess(answer, guesses);
-            Console.WriteLine("{0} I was thinking of {1}",result, answer);
+            Console.WriteLine("{0} I was thinking of {1}",result, answer); //Inform the user whether they have won or lost and what the correct answer was.
         }
     }
 }
